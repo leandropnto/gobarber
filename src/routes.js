327c8e41c78c8e6fe1d2b15f190da1af606af7be
lpinto <lpinto@crm-rj.gov.br>
2019-06-27 19:34:27 -0300

    Middlewares de validação; Configuração do multer; Criação do FileController

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import authMiddleware from './app/middlewares/auth';
+import {
+  createUserMiddleware,
+  updateUserMiddleare,
+} from './app/middlewares/users';
+
+import storeSessionMiddleware from './app/middlewares/sessions';
+
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', createUserMiddleware, UserController.store);
+routes.post('/sessions', storeSessionMiddleware, SessionController.store);
 
 /* Protege apenas a rota de atualização */
 routes.use(authMiddleware);
-routes.put('/users', UserController.update);
+routes.put('/users', updateUserMiddleare, UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
