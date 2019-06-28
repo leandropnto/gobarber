52529f0f0af609a5ff33e1edb99c5c7bd03859bd
Leandro Pinto <leandro.pnto@gmail.com>
2019-06-27 23:51:24 -0300

    Atualizacao avatar; Lista provider;

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,4 +26,5 @@ routes.use(authMiddleware);
 routes.put('/users', updateUserMiddleare, UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 export default routes;
