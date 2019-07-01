6ebc84cb6b494c57fa24d89129fce83f1eeb162c
lpinto <lpinto@crm-rj.gov.br>
2019-07-01 17:25:37 -0300

    Configurando envio de e-mail com template;

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -105,7 +105,10 @@ class AppointmentController {
 
   async delete(req, res) {
     const appointment = await Appointment.findByPk(req.params.id, {
-      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+      include: [
+        { model: User, as: 'provider', attributes: ['name', 'email'] },
+        { model: User, as: 'user', attributes: ['name', 'email'] },
+      ],
     });
 
     if (!appointment)
@@ -130,7 +133,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento Cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
