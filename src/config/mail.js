a26e454a001191caf48fdc43f4cb51866b54d028
lpinto <lpinto@crm-rj.gov.br>
2019-07-01 16:22:25 -0300

    Envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6198cc43592178',
+    pass: '9df545ffea3953',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
