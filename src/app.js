ac4bab2db1fed1f62a6324ae3e478a2abcc342f6
Leandro Pinto <leandro.pnto@gmail.com>
2019-07-01 22:56:24 -0300

    Tratanto excecoes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+
 import routes from './routes';
 import './database';
+import sentryConfig from './config/sentry';
+
+import 'express-async-errors';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+
     this.midlewares();
     this.routes();
   }
 
   midlewares() {
+    // The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +29,12 @@ class App {
 
   routes() {
     this.server.use(routes);
+    // The error handler must be before any other error middleware
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {});
   }
 }
 export default new App().server;
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://3353ae29c59a4b639f047a22644040c9@sentry.io/1494687',
+};
